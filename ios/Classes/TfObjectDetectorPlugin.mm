f5191e1ce37a6ad73265176627dbf1da8aec41ee
Dmytro Poroshyn <dimonator009@gmail.com>
2025-03-04 02:44:43 +0200

    improved smoothens of the iOS

--- ios/Classes/TfObjectDetectorPlugin.mm
+++ ios/Classes/TfObjectDetectorPlugin.mm
@@ -139,63 +139,62 @@ void feedInputTensorFrame(const FlutterStandardTypedData* typedData, int* input_
 }
 
 NSMutableArray* parseResults(int num_results_per_class) {
-
-  assert(TfLiteInterpreterGetOutputTensorCount(interpreter) == 4);
-
-  NSMutableArray* results = [NSMutableArray array];
-
-  float* output_locations = TfLiteInterpreterGetOutputTensor(interpreter, 0)->data.f;
-  float* output_classes = TfLiteInterpreterGetOutputTensor(interpreter, 1)->data.f;
-  float* output_scores = TfLiteInterpreterGetOutputTensor(interpreter, 2)->data.f;
-  float* num_detections = TfLiteInterpreterGetOutputTensor(interpreter, 3)->data.f;
-
-
-  NSMutableDictionary* counters = [NSMutableDictionary dictionary];
-  for (int d = 0; d < *num_detections; d++)
-  {
-    const int detected_class = output_classes[d];
-    float score = output_scores[d];
     
-    if (score < threshold) continue;
+    assert(TfLiteInterpreterGetOutputTensorCount(interpreter) == 4);
     
-    NSMutableDictionary* res = [NSMutableDictionary dictionary];
-    NSString* class_name = labels[detected_class];
-    NSObject* counter = [counters objectForKey:class_name];
+    NSMutableArray* results = [NSMutableArray array];
     
-    if (counter) {
-      int countValue = [(NSNumber*)counter intValue] + 1;
-      if (countValue > num_results_per_class) {
-        continue;
-      }
-      [counters setObject:@(countValue) forKey:class_name];
-    } else {
-      [counters setObject:@(1) forKey:class_name];
+    float* output_locations = TfLiteInterpreterGetOutputTensor(interpreter, 0)->data.f;
+    float* output_classes = TfLiteInterpreterGetOutputTensor(interpreter, 1)->data.f;
+    float* output_scores = TfLiteInterpreterGetOutputTensor(interpreter, 2)->data.f;
+    float* num_detections = TfLiteInterpreterGetOutputTensor(interpreter, 3)->data.f;
+    
+    
+    NSMutableDictionary* counters = [NSMutableDictionary dictionary];
+    for (int d = 0; d < *num_detections; d++)
+    {
+        const int detected_class = output_classes[d];
+        float score = output_scores[d];
+        
+        if (score < threshold) continue;
+        
+        NSMutableDictionary* res = [NSMutableDictionary dictionary];
+        NSString* class_name = labels[detected_class];
+        NSObject* counter = [counters objectForKey:class_name];
+        
+        if (counter) {
+            int countValue = [(NSNumber*)counter intValue] + 1;
+            if (countValue > num_results_per_class) {
+                continue;
+            }
+            [counters setObject:@(countValue) forKey:class_name];
+        } else {
+            [counters setObject:@(1) forKey:class_name];
+        }
+        
+        [res setObject:@(score) forKey:@"score"];
+        [res setObject:class_name forKey:@"label"];
+        
+        const float ymin = fmax(0, output_locations[d * 4]);
+        const float xmin = fmax(0, output_locations[d * 4 + 1]);
+        const float ymax = output_locations[d * 4 + 2];
+        const float xmax = output_locations[d * 4 + 3];
+        
+        NSMutableDictionary* rect = [NSMutableDictionary dictionary];
+        [rect setObject:@(xmin) forKey:@"left"];
+        [rect setObject:@(ymin) forKey:@"top"];
+        
+        [rect setObject:@(xmax) forKey:@"right"];
+        [rect setObject:@(ymax) forKey:@"bottom"];
+        
+        [res setObject:rect forKey:@"rect"];
+        [results addObject:res];
     }
-    
-    [res setObject:@(score) forKey:@"score"];
-    [res setObject:class_name forKey:@"label"];
-    
-    const float ymin = fmax(0, output_locations[d * 4]);
-    const float xmin = fmax(0, output_locations[d * 4 + 1]);
-    const float ymax = output_locations[d * 4 + 2];
-    const float xmax = output_locations[d * 4 + 3];
-
-    NSMutableDictionary* rect = [NSMutableDictionary dictionary];
-    [rect setObject:@(xmin) forKey:@"left"];
-    [rect setObject:@(ymin) forKey:@"top"];
-      
-    [rect setObject:@(xmax) forKey:@"right"];
-    [rect setObject:@(ymax) forKey:@"bottom"];
-    
-    [res setObject:rect forKey:@"rect"];
-    [results addObject:res];
-  }
-  return results;
+    return results;
 }
 
 void detect(NSDictionary* args, FlutterResult result) {
     const FlutterStandardTypedData* typedData = args[@"bytesList"][0];
-    const NSString* model = args[@"model"];
     const int image_height = [args[@"imageHeight"] intValue];
     const int image_width = [args[@"imageWidth"] intValue];
     const float input_mean = [args[@"imageMean"] floatValue];
@@ -210,12 +209,6 @@ void detect(NSDictionary* args, FlutterResult result) {
         return result(empty);
     }
     
-    int input_size;
-    int image_channels = 4;
-    
-    uint8_t* in = (uint8_t*)[[typedData data] bytes];
-    feedInputTensor(in, &input_size, image_height, image_width, image_channels, input_mean, input_std);
-    
     runDetection(args, ^(TfLiteStatus status) {
         if (status != kTfLiteOk) {
             NSLog(@"Failed to invoke!");
@@ -226,29 +219,33 @@ void detect(NSDictionary* args, FlutterResult result) {
 }
 
 void runDetection(NSDictionary* args, TfLiteStatusCallback cb) {
-    const bool asynch = [args[@"asynch"] boolValue];
-    if (asynch) {
-        interpreter_busy = true;
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
-            
-            TfLiteStatus status = TfLiteInterpreterInvoke(interpreter);
-            
-            dispatch_async(dispatch_get_main_queue(), ^(void){
-                interpreter_busy = false;
-                cb(status);
-            });
-        });
-    } else {
+    interpreter_busy = true;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+        
+        const FlutterStandardTypedData* typedData = args[@"bytesList"][0];
+        const int image_height = [args[@"imageHeight"] intValue];
+        const int image_width = [args[@"imageWidth"] intValue];
+        const float input_mean = [args[@"imageMean"] floatValue];
+        const float input_std = [args[@"imageStd"] floatValue];
+        
+        int input_size;
+        int image_channels = 4;
+        
+        uint8_t* in = (uint8_t*)[[typedData data] bytes];
+        feedInputTensor(in, &input_size, image_height, image_width, image_channels, input_mean, input_std);
         
         TfLiteStatus status = TfLiteInterpreterInvoke(interpreter);
         
-        cb(status);
-    }
+        dispatch_async(dispatch_get_main_queue(), ^(void){
+            interpreter_busy = false;
+            cb(status);
+        });
+    });
 }
 
 void close() {
-  interpreter = nullptr;
-  delegate = nullptr;
-  model = NULL;
-  labels = nullptr;
+    interpreter = nullptr;
+    delegate = nullptr;
+    model = NULL;
+    labels = nullptr;
 }
